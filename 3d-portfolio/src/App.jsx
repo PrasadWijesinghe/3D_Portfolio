1e6795f16816e5b659a76665a11d6c6dedee66b2
PrasadWijesinghe <wmprasadwijesinghe@gmail.com>
2025-04-20 23:58:52 +0530

    third commit

--- 3d-portfolio/src/App.jsx
+++ 3d-portfolio/src/App.jsx
@@ -4,6 +4,8 @@ const App = () => {
   return (
     <div>
       Welcome to my 3D Portfolio! <br />
+      This is a simple 3D portfolio built with React and Three.js. <br />
+      You can view my projects and skills in 3D space. <br />
     </div>
   )
 }

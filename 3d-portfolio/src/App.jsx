3c6bb555ee42a21163ed1557027c67469f89f06d
PrasadWijesinghe <wmprasadwijesinghe@gmail.com>
2025-04-30 22:40:05 +0530

    commit

--- 3d-portfolio/src/App.jsx
+++ 3d-portfolio/src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
     <div className='h-screen w-screen flex items-center justify-center bg-gray-900 text-white'>
       Welcome to my 3D Portfolio! <br />
       This is a simple 3D portfolio built with React and Three.js. <br />
-      You can find the source code on my GitHub. <br />
+     
       
       
       
